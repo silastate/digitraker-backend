7e4146ee3e026a2abd3bfd500f03777c71dfe137
Julio Locatelli Piva <juliolpiva@hotmail.com>
2021-08-12 21:05:28 +0200

    Little improvements in generateAlarmReport

--- lambdas/generateAlarmReport/generateAlarmReport/app.js
+++ lambdas/generateAlarmReport/generateAlarmReport/app.js
@@ -5,19 +5,47 @@ AWS.config.update({ region: 'us-east-2' });
 
 const dynamo = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
+const HEADERS = {
+  headers: {
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+  },
+};
+
 exports.lambdaHandler = async (event) => {
-  const request = event.body;
+  try {
+    const payload = JSON.parse(event.body);
 
-  const params = {
-    TableName: 'Alarms',
-    FilterExpression: 'contains(:txids, #txid) AND alarmId BETWEEN :start AND :stop',
-    ExpressionAttributeNames: { '#txid': 'txid' },
-    ExpressionAttributeValues: {
-      ':txids': request.txids,
-      ':start': parseInt(request.time1),
-      ':stop': parseInt(request.time2),
-    },
-  };
+    console.log('Payload', payload);
 
-  return await dynamo.scan(params).promise();
+    const params = {
+      TableName: 'Alarms',
+      FilterExpression: 'contains(:txids, #txid) AND alarmId BETWEEN :start AND :stop',
+      ExpressionAttributeNames: { '#txid': 'txid' },
+      ExpressionAttributeValues: {
+        ':txids': payload.txids,
+        ':start': payload.time1,
+        ':stop': payload.time2,
+      },
+    };
+
+    const results = await dynamo.scan(params).promise();
+
+    const response = {
+      statusCode: 200,
+      headers: HEADERS,
+      body: JSON.stringify(results),
+    };
+
+    return response;
+  } catch (err) {
+    const error = {
+      statusCode: 400,
+      headers: HEADERS,
+      body: JSON.stringify(err),
+    };
+
+    return error;
+  }
 };

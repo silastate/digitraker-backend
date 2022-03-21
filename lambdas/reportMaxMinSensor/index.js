ad3fae828bfb9d5be86cf9e311f0d0285165311d
Julio <julio.piva@morressier.com>
2022-03-21 19:39:21 +0100

    Fixes max min timezones

--- lambdas/reportMaxMinSensor/index.js
+++ lambdas/reportMaxMinSensor/index.js
@@ -6,13 +6,12 @@ AWS.config.update({ region: 'us-east-2' });
 const dynamo = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: {},
-  };
+  let response = {};
 
   try {
-    const payload = event?.body?.txid ? event.body : JSON.parse(event.body);
+    console.log('event', event);
+
+    const payload = event?.txid ? event : JSON.parse(event);
 
     const query = {
       TableName: 'Readings',
@@ -28,7 +27,9 @@ exports.handler = async (event) => {
     const readings = await dynamo.query(query).promise();
 
     const groupedByDay = _.groupBy(readings.Items, (reading) => {
-      return Math.ceil(reading.createdAt / (24 * 60 * 60 * 1000));
+      return new Date(reading.createdAt)
+        .toLocaleString('en-US', { timeZone: 'EST' })
+        .substring(0, 9);
     });
 
     const wideValues = Object.values(groupedByDay).map((day) => ({
@@ -36,7 +37,7 @@ exports.handler = async (event) => {
       min: _.minBy(day, (reading) => reading.value),
     }));
 
-    response.body = JSON.stringify(wideValues);
+    response = wideValues;
   } catch (err) {
     console.log(err);
   }
